5fdbfb51fc437c545937fe3fe2cd4d85362af048
hariacharya7 <118670580+hariacharya7@users.noreply.github.com>
2023-02-14 20:12:10 +0545

    Update program5_WAP To input mask of 5 subject and find out your position.c

--- program5_WAP To input mask of 5 subject and find out your position.c
+++ program5_WAP To input mask of 5 subject and find out your position.c
@@ -1,1 +1,26 @@
 
+/******************************************************************************
+WAP To input mask of 5 subject and find out your position
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c,d,e,student_percent,marks_sum;
+    printf("Enter marks of 5 subjects respectively.");
+    scanf("%d %d %d %d %d ", &a,&b,&c,&d,&e);
+    marks_sum= a+b+c+d+e;
+    student_percent= (marks_sum/5);
+    if(student_percent>=80){
+        printf("You have first division.");
+    }else if(student_percent>=70){
+        printf("You have second division");
+    }else if(student_percent>=60){
+        printf("You have third division");
+    }else{
+        printf("Please try your hard since you failed the exam");
+    }
+    
+
+    return 0;
+}
