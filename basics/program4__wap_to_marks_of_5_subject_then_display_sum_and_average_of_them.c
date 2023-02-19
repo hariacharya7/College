c2e3e825fb018996c3ab841262fb17cebcd409b7
Hari Acharya <118670580+hariacharya7@users.noreply.github.com>
2023-02-20 00:39:29 +0545

    Update program4__wap_to_marks_of_5_subject_then_display_sum_and_average_of_them.c

--- basics/program4__wap_to_marks_of_5_subject_then_display_sum_and_average_of_them.c
+++ basics/program4__wap_to_marks_of_5_subject_then_display_sum_and_average_of_them.c
@@ -3,4 +3,9 @@
 int main(){
     int a,b,c,d,e;
     printf("enter marks in five subjects respectively.");
+    scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
+    sum=a+b+c+d+e;
+    average=sum/5;
+    printf("%d",sum);
+    printf("%d",average);
 }
